f3d1bc0c07c73c43ef64955132d243a854124b08
Dakingrai <dakingrae@gmail.com>
2023-04-12 13:03:43 -0400

    settings updated!

--- user_study/settings.py
+++ user_study/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-wfj+e0%v@tkny4xl%g)o22t=m*^u&=fe)wl%d4t@@4jkwx+j7*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
